65512854dcf6bd3debae473ac89b24a2861acfb6
Johann Barnard <johann.b@telus.net>
2023-03-07 16:28:59 -0700

    Add a randomly-generated map
    Oh yeah and start making the actual game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,208 @@
-fn main() {
-    println!("Hello, world!");
+use std::cmp::{max, min};
+
+use rltk::{GameState, Rltk, RltkBuilder, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs::Component;
+
+/// Tracks the location of an entity.
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+/// Provides a CP437 character and fg/bg colors to render an entity with.
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+/// Indicates that an entity is the Player character.
+#[derive(Component, Debug)]
+struct Player;
+
+/// All possible tile types.
+#[derive(PartialEq, Eq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+/// Convert (x, y) coordinates to an offset in an array of tiles.
+///
+/// Assumes that each row of the screen is 80 tiles wide. Therefore, each time
+/// `y` increases by 1, the output increases by `80`.
+pub const fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+/// Create a simple game map.
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make all boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Randomly place a bunch of other walls in other places.
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _ in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+
+        // Don't put a wall where the player spawns!
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+/// Draw a game map on screen.
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending on the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0.0, 0.0, 0.0),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0.0, 0.0, 0.0),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        // Next coord
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+/// Global game state.
+struct State {
+    ecs: World,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self { ecs: World::new() }
+    }
+}
+
+impl State {
+    /// Runs all ECS systems for one ECS tick.
+    fn run_systems(&mut self) {
+        // no-op
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx); // handle player input
+        self.run_systems(); // tick the ECS
+
+        // Render the map
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        // Render anything that has a position
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+/// Try to move the player by a certain delta vector, if the ECS contains
+/// at least one entity that has both the [`Position`] and [`Player`] components.
+///
+/// Will prevent the player from moving off-screen or through walls.
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
+    }
+}
+
+/// Handle player input.
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left | VirtualKeyCode::A => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right | VirtualKeyCode::D => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up | VirtualKeyCode::W => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down | VirtualKeyCode::S => try_move_player(0, 1, &mut gs.ecs),
+
+            _ => {}
+        },
+    }
+}
+
+fn main() -> rltk::BError {
+    let context = RltkBuilder::simple80x50()
+        .with_title("Hello RLTK World")
+        .with_fps_cap(60.0)
+        .build()?;
+
+    let mut gs = State::default();
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
+
+    // Create the player
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player)
+        .build();
+
+    rltk::main_loop(context, gs)
 }
